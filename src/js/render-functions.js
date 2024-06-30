ac5f9c9eeab5bd47c7530bab77edaf1d0093f681
Olya Paslavska <157712045+OlaPaslavska@users.noreply.github.com>
2024-06-30 15:14:36 +0200

    render

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -7,6 +7,7 @@ export const refs = {
     button: document.querySelector('button'),
     gallery: document.querySelector('.gallery'),
     loader: document.querySelector('.loader'),
+      loadMoreBtn: document.querySelector('.load-more-btn'),
 };
 
 export async function imagesTemplate(imagesObj) {
